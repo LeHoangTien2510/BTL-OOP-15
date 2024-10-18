35cfe19802e4e4a7881f79d3c7029655cf2ae45d
Le-Hoang-Tien <tienlehoanghn2005@gmail.com>
2024-10-18 16:28:22 +0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module org.example.btloop15 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.example.btloop15 to javafx.fxml;
+    exports org.example.btloop15;
+}
