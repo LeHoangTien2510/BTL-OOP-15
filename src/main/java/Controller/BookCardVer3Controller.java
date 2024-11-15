6dd0a0a6b10bc8cbec0997ede375943bedb1e90e
Le-Hoang-Tien <tienlehoanghn2005@gmail.com>
2024-11-15 17:23:49 +0700

    return sách thành công

--- src/main/java/Controller/BookCardVer3Controller.java
+++ src/main/java/Controller/BookCardVer3Controller.java
@@ -1,13 +1,17 @@
 package Controller;
 
-import Objects.Book;
-import Objects.MyListener;
+import Objects.*;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class BookCardVer3Controller {
     @FXML
@@ -30,7 +34,10 @@ public class BookCardVer3Controller {
 
     private Book book;
     private MyListener myListener;
-
+    Connection conn = SqliteConnection.Connector();
+    User currentUser = Login.getCurrentUser();
+    String findTitle;
+    int id;
     @FXML
     private void clickBook(Event event) {
         myListener.onClickListener(book);
@@ -43,10 +50,39 @@ public class BookCardVer3Controller {
         String imagePath = book.getImageSrc();
         Image image = new Image(getClass().getResourceAsStream(imagePath));
         bookImage.setImage(image);
-
         bookTitle.setText(book.getTitle());
         bookAuthor.setText(book.getAuthor());
         bookGenre.setText(book.getGenre());
         borrowedDate.setText(book.getBorrowedDate());
+        findTitle = bookTitle.getText();
+        id = book.getBookIdFromBookCard(findTitle);
+    }
+
+    @FXML
+    private void handleReturnButton(Event event) throws SQLException {
+        String deleteBorrowedBooksQuery = "DELETE FROM borrowed_books WHERE user_id = ? AND book_id = ?";
+        try (PreparedStatement preparedStatement = conn.prepareStatement(deleteBorrowedBooksQuery)) {
+            // Gán giá trị cho các tham số
+            preparedStatement.setInt(1, currentUser.getIdFromDb());
+            preparedStatement.setInt(2, id);
+
+            int result = preparedStatement.executeUpdate();
+            if (result > 0) {
+                showAlert(Alert.AlertType.INFORMATION, "Return Book", "Return " + findTitle + " Successfully");
+            } else {
+                showAlert(Alert.AlertType.ERROR, "Return failed", "Return" + findTitle + " Failed");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Failed to return book.");
+        }
+    }
+
+    private void showAlert(Alert.AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 }
