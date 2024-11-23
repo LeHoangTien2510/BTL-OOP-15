96716a8e0dfd52d73251a82518ba31129197eee2
Le-Hoang-Tien <tienlehoanghn2005@gmail.com>
2024-11-23 09:48:47 +0700

    thêm lại video

---
+++ src/main/java/Controller/UserInterfaceController.java
@@ -0,0 +1,218 @@
+package Controller;
+
+import javafx.beans.binding.Bindings;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
+import Objects.User;
+import Objects.Login;
+import javafx.fxml.Initializable;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.util.Duration;
+import java.net.URL;
+import java.io.IOException;
+import java.util.*;
+
+public class UserInterfaceController implements Initializable {
+    @FXML
+    private AnchorPane UserView;
+
+    @FXML
+    private Button UserProfileDisplayNameButton;
+    User currentUser = Login.getCurrentUser();
+
+    @FXML
+    private Button DashBoardButton;
+
+    @FXML
+    private Button borrowedBooksButton;
+
+    @FXML
+    private Button handleLogOutButton;
+
+    @FXML
+    private Button profileButton;
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    private MediaView media;
+
+    private MediaPlayer mediaPlayer;
+
+    @FXML
+    public void initialize(URL location, ResourceBundle resources) {
+        if (currentUser != null) {
+            UserProfileDisplayNameButton.setText(currentUser.getName());
+        }
+
+        DashBoardButton.styleProperty().bind(
+                Bindings.when(DashBoardButton.hoverProperty())
+                        .then("-fx-background-color: #1e4f5f; -fx-text-fill: #FFFFFF;") // Màu nền khi hover và màu chữ
+                        .otherwise("-fx-background-color: #0e3746; -fx-text-fill: #FFFFFF;") // Màu nền mặc định và màu chữ
+        );
+
+        searchButton.styleProperty().bind(
+                Bindings.when(searchButton.hoverProperty())
+                        .then("-fx-background-color: #1e4f5f; -fx-text-fill: #FFFFFF;") // Màu nền khi hover và màu chữ
+                        .otherwise("-fx-background-color: #0e3746; -fx-text-fill: #FFFFFF;") // Màu nền mặc định và màu chữ
+        );
+
+        searchButton.styleProperty().bind(
+                Bindings.when(searchButton.hoverProperty())
+                        .then("-fx-background-color: #1e4f5f; -fx-text-fill: #FFFFFF;") // Màu nền khi hover và màu chữ
+                        .otherwise("-fx-background-color: #0e3746; -fx-text-fill: #FFFFFF;") // Màu nền mặc định và màu chữ
+        );
+
+        borrowedBooksButton.styleProperty().bind(
+                Bindings.when(borrowedBooksButton.hoverProperty())
+                        .then("-fx-background-color: #1e4f5f; -fx-text-fill: #FFFFFF;") // Màu nền khi hover và màu chữ
+                        .otherwise("-fx-background-color: #0e3746; -fx-text-fill: #FFFFFF;") // Màu nền mặc định và màu chữ
+        );
+
+        profileButton.styleProperty().bind(
+                Bindings.when(profileButton.hoverProperty())
+                        .then("-fx-background-color: #1e4f5f; -fx-text-fill: #FFFFFF;") // Màu nền khi hover và màu chữ
+                        .otherwise("-fx-background-color: #0e3746; -fx-text-fill: #FFFFFF;") // Màu nền mặc định và màu chữ
+        );
+        String videoPath = getClass().getResource("/arisu bị câm.mp4").toExternalForm();
+
+        // Tạo đối tượng Media và MediaPlayer
+        Media mediaFile = new Media(videoPath);
+        mediaPlayer = new MediaPlayer(mediaFile);
+
+        // Gắn MediaPlayer vào MediaView
+        media.setMediaPlayer(mediaPlayer);
+
+        // Tự động phát video ngay lập tức
+        mediaPlayer.setAutoPlay(true);
+        mediaPlayer.play();
+        mediaPlayer.setOnEndOfMedia(() -> mediaPlayer.seek(Duration.ZERO));
+    }
+
+    @FXML
+    private void handleLogOutButtonAction(ActionEvent event) {
+        try {
+            Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+
+            // Hiển thị hộp thoại xác nhận đăng xuất
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.initOwner(stage);
+            alert.setTitle("Đăng xuất");
+            alert.setHeaderText("Bạn muốn đăng xuất?");
+            alert.setContentText("Bạn có muốn lưu dữ liệu trước khi đăng xuất?");
+
+            // Chờ người dùng phản hồi
+            Optional<ButtonType> result = alert.showAndWait();
+
+            if (result.isPresent() && result.get() == ButtonType.OK) {
+                // Tải giao diện đăng nhập
+                stopMediaPlayer();
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/library/login.fxml"));
+                Parent loginRoot = loader.load();
+
+                stage.setScene(new Scene(loginRoot));
+                stage.setTitle("Đăng nhập");
+
+                stage.centerOnScreen();
+                stage.show();
+            }
+        } catch (IOException e) {
+            showAlert(Alert.AlertType.ERROR, "Lỗi", "Không thể tải giao diện đăng nhập.");
+            e.printStackTrace();
+        }
+    }
+
+    private void showAlert(Alert.AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    @FXML
+    private void handleDashBoardButtonAction(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/library/DashBoard.fxml"));
+            AnchorPane dashBoardView = fxmlLoader.load();
+
+            UserView.getChildren().clear();
+
+            UserView.getChildren().add(dashBoardView);
+        } catch (IOException e) {
+            showAlert(Alert.AlertType.ERROR, "Lỗi", "Không thể tải giao diện Dashboard.");
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void handleBorrowingBookButton(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/library/BorrowedBooks.fxml"));
+            AnchorPane dashBoardView = fxmlLoader.load();
+
+            UserView.getChildren().clear();
+
+            UserView.getChildren().add(dashBoardView);
+        } catch (IOException e) {
+            showAlert(Alert.AlertType.ERROR, "Lỗi", "Không thể tải giao diện Borrowing_books.");
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void handleUserProfileButtonAction(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/library/UserProfile.fxml"));
+            AnchorPane dashBoardView = fxmlLoader.load();
+
+            UserView.getChildren().clear();
+
+            UserView.getChildren().add(dashBoardView);
+        } catch (IOException e) {
+            showAlert(Alert.AlertType.ERROR, "Lỗi", "Không thể tải giao diện Dashboard.");
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void handleSearchBookButtonAction(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/library/SearchBook.fxml"));
+            AnchorPane dashBoardView = fxmlLoader.load();
+
+            UserView.getChildren().clear();
+
+            UserView.getChildren().add(dashBoardView);
+        } catch (IOException e) {
+            showAlert(Alert.AlertType.ERROR, "Lỗi", "Không thể tải giao diện Search.");
+            e.printStackTrace();
+        }
+    }
+
+    public void stopMediaPlayer() {
+        if (mediaPlayer != null) {
+            try {
+                mediaPlayer.stop();    // Dừng nhạc
+                mediaPlayer.dispose(); // Giải phóng tài nguyên
+            } catch (IllegalStateException e) {
+                System.err.println("MediaPlayer không ở trạng thái hợp lệ: " + e.getMessage());
+            } finally {
+                mediaPlayer = null; // Đảm bảo không còn tham chiếu
+            }
+        }
+    }
+}
